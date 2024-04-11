aa5112bff7b0ed2b92dcc807b0a61b1acdace28b
Italo Silva <italo@maleldil.com>
2024-04-11 10:03:57 +0100

    Use leading comma for scripts

--- python/src/scripts/estimate_tokens.py
+++ python/src/scripts/estimate_tokens.py
@@ -9,9 +9,7 @@ encoding = tiktoken.get_encoding("cl100k_base")
 
 @app.command()
 def calculate(file: typer.FileText = typer.Argument(..., help="Path to the file")):
-    """
-    Calculates a more accurate GPT-4 token count of a text file using tiktoken.
-    """
+    """Calculates a more accurate GPT-4 token count of a text file using tiktoken."""
     text = file.read()
     tokens = encoding.encode(text)
 
