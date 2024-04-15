e6779acf68bb37c9dcc1a155bcff5be535eda9cb
Italo Silva <italo@maleldil.com>
2024-04-15 16:15:33 +0100

    json-shuf: Add script to shuffle JSON files

---
+++ python/src/scripts/json_shuf.py
@@ -0,0 +1,56 @@
+"""Given a JSON file containing a list, shuffles the contents of the list.
+
+The input JSON file can be provided as an argument or piped in through stdin.
+The output JSON file can be provided as an argument or piped out through stdout.
+
+A random seed can be provided to ensure reproducibility.
+"""
+
+import argparse
+import json
+import random
+import sys
+from pathlib import Path
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__.splitlines()[0])
+    parser.add_argument(
+        "input",
+        type=Path,
+        nargs="?",
+        help="Path to input JSON file. If not provided, reads from stdin.",
+    )
+    parser.add_argument(
+        "output",
+        type=Path,
+        nargs="?",
+        help="Path to output JSON file. If not provided, prints to stdout.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=0,
+        help="Random seed used for shuffling. Default: %(default)s.",
+    )
+    args = parser.parse_args()
+
+    if args.input is None:
+        data = json.load(sys.stdin)
+    else:
+        data = json.loads(args.input.read_text())
+
+    if not isinstance(data, list):
+        print("Shuffle requires the JSON to be a list", file=sys.stderr)
+
+    random.seed(args.seed)
+    random.shuffle(data)
+
+    if args.output is None:
+        json.dump(data, sys.stdout, indent=2)
+    else:
+        args.output.write_text(json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    main()
