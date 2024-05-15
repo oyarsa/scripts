fd5621f18f45e57ffdd1df32b0f76726eb14f86e
Italo Silva <italo@maleldil.com>
2024-05-15 12:14:36 +0100

    toggle-theme: Script to toggle between light/dark themes

---
+++ python/src/scripts/toggle_theme.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+"""Toggle between light and dark themes in various config files.
+
+Currently supports the following config files:
+- Neovim
+- Kitty
+- Delta
+- Tmux
+- Fzf/fd/eza/LS_COLORS-supporting programs
+- Bat
+- Fish
+- Lazygit
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+from enum import Enum
+from pathlib import Path
+
+import yaml
+
+config_files = {
+    # Neovim
+    "~/.config/nvim/lua/config/plugins.lua": {
+        "light": 'theme = "catppuccin-latte"',
+        "dark": 'theme = "catppuccin-mocha"',
+    },
+    # Kitty
+    "~/.config/kitty/kitty.conf": {
+        "light": "Catppuccin Latte",
+        "dark": "Catppuccin Mocha",
+    },
+    # Delta
+    "~/.config/git/config": {
+        "light": "catppuccin-mocha",
+        "dark": "catppuccin-latte",
+    },
+    # Tmux
+    "~/.config/tmux/tmux.conf": {
+        "light": 'catppuccin_flavour = "latte"',
+        "dark": 'catppuccin_flavour = "mocha"',
+    },
+    # Fzf/fd/eza/LS_COLORS-supporting programs
+    "~/.config/fish/conf.d/colours.fish": {
+        "light": "theme catppuccin-latte",
+        "dark": "theme catppuccin-mocha",
+    },
+    # Bat
+    "~/.config/bat/config": {
+        "light": 'theme="Catppuccin Latte',
+        "dark": 'theme="Catppuccin Mocha',
+    },
+}
+
+
+def fish_theme(theme: str) -> None:
+    """Set the fish theme through `fish_config` CLI."""
+    try:
+        subprocess.run(
+            ["fish", "-c", f'yes | fish_config theme save "{theme}"'], check=True
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Warning: failed to change fish theme: {e.output}")
+
+
+def lazygit_theme(theme: str) -> None:
+    """Read theme from a file and updates the config file's `gui` key."""
+    config_path = Path("~/.config/lazygit").expanduser()
+
+    config = yaml.safe_load((config_path / "config.yml").read_text())
+    theme = yaml.safe_load((config_path / "themes" / f"{theme}.yml").read_text())
+
+    config["gui"] = theme
+    config_path.write_text(yaml.dump(config))
+
+
+# Conifgurations that can't be done by simple string replacement
+config_funcs = {
+    fish_theme: {
+        "light": "Catppuccin Latte",
+        "dark": "Catppuccin Mocha",
+    },
+    lazygit_theme: {
+        "light": "catppuccin-latte",
+        "dark": "catppuccin-mocha",
+    },
+}
+
+
+class Theme(Enum):
+    CATPPUCCIN_LATTE = "catppuccin-latte"
+    CATPPUCCIN_MOCHA = "catppuccin-mocha"
+
+
+def replace_in_file(file_path: Path, old: str, new: str) -> int:
+    content = file_path.expanduser().read_text()
+    count = content.count(old)
+    changed = content.replace(old, new)
+    file_path.write_text(changed)
+    return count
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=__doc__.splitlines()[0],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--warn-multiple",
+        action=argparse.BooleanOptionalAction,
+        type=bool,
+        default=True,
+        help="Warn if more than one replacement is made in a file",
+    )
+    args = parser.parse_args()
+
+    valid_themes = ", ".join(f"'{theme.value}'" for theme in Theme)
+    theme_msg = f"Choose from: {valid_themes}"
+    try:
+        current_theme = Theme(os.environ["ILS_COLOUR_THEME"])
+    except KeyError:
+        print(f"ILS_COLOUR_THEME environment variable is not set. {theme_msg}")
+        sys.exit(1)
+    except ValueError:
+        print(f"Invalid theme. {theme_msg}")
+        sys.exit(1)
+
+    match current_theme:
+        case Theme.CATPPUCCIN_LATTE:
+            current, new = "light", "dark"
+        case Theme.CATPPUCCIN_MOCHA:
+            current, new = "dark", "light"
+
+    for file, values in config_files.items():
+        count = replace_in_file(Path(file), values[current], values[new])
+        if args.warn_multiple and count > 1:
+            print(f"Warning: more than one replacement made in {file}.")
+
+    for func, values in config_funcs.items():
+        func(values[new])
+
+
+if __name__ == "__main__":
+    main()
