963c1926eb105003fa168f37bfd13ea26390d6f8
Italo Silva <italo@maleldil.com>
2024-12-02 22:11:20 +0000

    estimate_tokens: Improve documentation and usability

--- python/src/scripts/estimate_tokens.py
+++ python/src/scripts/estimate_tokens.py
@@ -1,17 +1,37 @@
+"""Estimate the number of tokens in the input file (or stdin) using the GPT-4 tokeniser."""
+
+import sys
+from pathlib import Path
+from typing import Annotated
+
 import tiktoken
 import typer
 
-app = typer.Typer()
+# GPT-4 and later tokeniser.
+_GPT_TOKENISER = tiktoken.get_encoding("cl100k_base")
 
-# Let's use a reasonably common GPT-4 encoding
-encoding = tiktoken.get_encoding("cl100k_base")
+app = typer.Typer(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    add_completion=False,
+    rich_markup_mode="rich",
+    pretty_exceptions_show_locals=False,
+    no_args_is_help=True,
+)
 
 
-@app.command()
-def calculate(file: typer.FileText = typer.Argument("-", help="Path to the file")):
-    """Calculates a more accurate GPT-4 token count of a text file using tiktoken."""
-    text = file.read()
-    tokens = encoding.encode(text)
+@app.command(help=__doc__)
+def calculate(
+    file: Annotated[
+        Path | None,
+        typer.Argument(help="Path to the file. If not provided, use stdin."),
+    ] = None,
+):
+    if file is None:
+        text = sys.stdin.read()
+    else:
+        text = file.read_text()
+
+    tokens = _GPT_TOKENISER.encode(text)
 
     print(len(tokens))
 
